ff1ce0929e557cdd653215c6f07297e08bbe7658
fikrinaufaland_ <fikrinaufal411@gmail.com>
2025-02-08 21:27:25 +0700

    week1

---
+++ FikriNaufalAndrasito_79229_WEEK1/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Informatika</Text>
+      <Text>Fikri Naufal Andrasito - 00000079229</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
